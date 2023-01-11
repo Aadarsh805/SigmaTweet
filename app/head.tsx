531dd54be8927ae2fc076840ef7fd2db0cdb577a
aadarsh805 <aadarshthakur80501@gmail.com>
2023-01-12 02:20:21 +0530

    initialized deps

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>SigmaTweet</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="An Upgraded version of TweetSage" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-8xl font-bold underline bg-gray-500">Hello, Next.js!</h1>;
+}
